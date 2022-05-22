b6cd35cf6f669b2d7166223ac1fac69de9b8c21a
codenoobies <kevindubidaam@gmail.com>
2022-05-23 00:39:42 +0700

    created function navigation adaptability

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
 // Hamburger Icon
 const hamburger = document.querySelector(".button");
 const navigation = document.querySelector(".nav-links");
-let navbarLinkActive;
-function getLinks() {
-    navbarLinkActive = location.href.split("#")[1];
-    console.log(navbarLinkActive);
-}
-
 function navbar() {
     hamburger.classList.toggle("active");
     navigation.classList.toggle("height-animation");
-    setTimeout(getLinks, 100);
 }
 
 hamburger.addEventListener("click", navbar);
 
 navigation.addEventListener("click", navbar);
+
+// Adaptability Navigation
+let navbarLinkActive;
+const navlinks = document.querySelectorAll("nav .nav-links ul li a");
+
+// Element Variable
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+
+function removeClass(element) {
+    if (navbarLinkActive) {
+        const temp = navbarLinkActive?.getAttribute("href").split("#")[1];
+        if (!(temp === element)) {
+            console.log("hello");
+            navbarLinkActive.classList.remove("active");
+        }
+    }
+}
+
+function addClass(target) {
+    navlinks.forEach((el) => {
+        const link = el.getAttribute("href").split("#")[1];
+
+        if (link === target.id) {
+            if (navbarLinkActive) {
+                removeClass(link);
+            }
+
+            el.classList.add("active");
+            navbarLinkActive = el;
+            return;
+        }
+    })
+}
+
+document.addEventListener("scroll", function () {
+    const clientLocation = document.querySelector("html").scrollTop;
+    if (clientLocation >= home.offsetTop && clientLocation <= about.offsetTop - 200) {
+        addClass(home);
+    } else if (clientLocation > about.offsetTop - 100) {
+        addClass(about);
+    }
+
+
+})
+
